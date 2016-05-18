0e2d6283998ee78ab98fbabdbeefcfba07b026aa
afternone <afternone@163.com>
2016-05-18 19:19:21 +0800

    simplify code

--- deps/build.jl
+++ deps/build.jl
@@ -4,24 +4,19 @@ using BinDeps
 libbnet = library_dependency("libbnet")
 
 libdir = BinDeps.libdir(libbnet)
-srcdir = BinDeps.srcdir(libbnet)
-downloadsdir = BinDeps.downloadsdir(libbnet)
-
-prefix=joinpath(BinDeps.depsdir(libbnet),"usr")
-uprefix = replace(replace(prefix,"\\","/"),"C:/","/c/")
-bnetsrcdir = joinpath(BinDeps.depsdir(libbnet),"src","binary_networks")
-provides(Sources,URI("https://raw.githubusercontent.com/afternone/CommunityDetection.jl/master/deps/binary_networks.tar.gz"), libbnet)
+srcdir = joinpath(BinDeps.srcdir(libbnet), "binary_networks")
+provides(Sources, URI("https://raw.githubusercontent.com/afternone/CommunityDetection.jl/master/deps/binary_networks.tar.gz"), libbnet)
 provides(BuildProcess,
     (@build_steps begin
         GetSources(libbnet)
         CreateDirectory(libdir)
         @build_steps begin
-            ChangeDirectory(bnetsrcdir)
-            FileRule(joinpath(prefix,"lib","libbnet.so"),@build_steps begin
+            ChangeDirectory(srcdir)
+            FileRule(joinpath(libdir, "libbnet.so"), @build_steps begin
                 `make`
                 `cp libbnet.so $libdir`
             end)
         end
-    end),libbnet)
+    end), libbnet)
 
 @BinDeps.install Dict(:libbnet => :libbnet)
--- src/LFR.jl
+++ src/LFR.jl
@@ -1,18 +1,20 @@
+__precompile__(true)
+
 module LFR
-using BinDeps
 
-
-depsjl = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-isfile(depsjl) ? include(depsjl) : error("LFR not properly ",
-"installed. Please run\nPkg.build(\"LFR\")")
+if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
+    include("../deps/deps.jl")
+else
+    error("LFR not properly installed. Please run Pkg.build(\"LFR\")")
+end
 
 export get_edgelist
 
 function get_edgelist(;excess=false,defect=false,N=1000,k=20,maxk=50,t1=2.0,t2=1.0,mu=0.1,on=0,om=0,minc=50,maxc=100,fixed_range=true,ca=-214741)
-	p = ccall((:benchmark, libbnet), Ptr{Cint}, (Bool, Bool, Cint, Float64, Cint, Float64, Float64, Float64, Cint, Cint, Cint, Cint, Bool, Float64), excess, defect, N, k, maxk, t1, t2, mu, on, om, minc,maxc,fixed_range,ca)
+	p = ccall((:benchmark, libbnet), Ptr{Cint}, (Bool,Bool,Cint,Float64,Cint,Float64,Float64,Float64,Cint,Cint,Cint,Cint,Bool,Float64), excess,defect,N,k,maxk,t1,t2,mu,on,om,minc,maxc,fixed_range,ca)
 	n = pointer_to_array(p,1)[1]
 	edgelist = Int[]
-	alldata = pointer_to_array(p,n)
+	alldata = pointer_to_array(p,n,true)
 	sepidx = 0
 	for i=2:n
 		if alldata[i] > 0
@@ -22,10 +24,7 @@ function get_edgelist(;excess=false,defect=false,N=1000,k=20,maxk=50,t1=2.0,t2=1
 			break
 		end
 	end
-	membership = Array(Vector{Int}, N)
-	for j=1:N
-		membership[j] = Vector{Int}()
-	end
+	membership = [Vector{Int}() for j=1:N]
 	j = 1
 	for k=sepidx+1:n
 		if alldata[k] > 0
@@ -37,5 +36,5 @@ function get_edgelist(;excess=false,defect=false,N=1000,k=20,maxk=50,t1=2.0,t2=1
 	edgelist, membership
 end
 
-																												end # module
+end # module
 
