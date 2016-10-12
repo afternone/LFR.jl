7e17ea5c2f16eb208bf9b0209684341702aa1fe1
afternone <afternone@163.com>
2016-10-12 15:17:24 +0800

    fix warning on v0.5

--- src/LFR.jl
+++ src/LFR.jl
@@ -12,9 +12,9 @@ export get_edgelist
 
 function get_edgelist(;excess=false,defect=false,N=1000,k=20,maxk=50,t1=2.0,t2=1.0,mu=0.1,on=0,om=0,minc=50,maxc=100,fixed_range=true,ca=-214741)
 	p = ccall((:benchmark, libbnet), Ptr{Cint}, (Bool,Bool,Cint,Float64,Cint,Float64,Float64,Float64,Cint,Cint,Cint,Cint,Bool,Float64), excess,defect,N,k,maxk,t1,t2,mu,on,om,minc,maxc,fixed_range,ca)
-	n = pointer_to_array(p,1)[1]
+	n = unsafe_wrap(p,1)[1]
 	edgelist = Int[]
-	alldata = pointer_to_array(p,n,true)
+	alldata = unsafe_wrap(p,n,true)
 	sepidx = 0
 	for i=2:n
 		if alldata[i] > 0
